351d2fac33ffef6c9e9647ca4445c15b3e48d9ae
gusttr <gusttr@outlook.com>
2024-04-20 00:11:01 -0300

    test: removing website domain

--- api/config.php
+++ api/config.php
@@ -11,7 +11,7 @@ $Website_Subfolder = 'api';
 
 // Place here your domain if steam authorize not working properly,
 // if its working good then leave it empty.
-$Website_Domain = 'https://skins-ten.vercel.app';
+$Website_Domain = '';
 
 // Enable this if you want categories else it will display all weapons.
 $Website_UseCategories = true;
